64b5ec143df8d67a406ea0f7e9c8f0bfae9b7d89
IreoluwaWisdom <140738967+IreoluwaWisdom@users.noreply.github.com>
2024-07-24 13:31:21 +0000

    gallery pictures outlined

--- fagtech/src/Gallery/gallery.js
+++ fagtech/src/Gallery/gallery.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import "../Styles/gallery.css";
+import pic1 from "../images/pic1.jpg";
+import pic2 from "../images/pic2.jpg";
+import pic3 from "../images/pic3.jpg";
+import pic4 from "../images/pic4.jpg";
+import pic5 from "../images/pic5.jpg";
+import pic6 from "../images/pic6.jpg";
 
 const Gallery = () => {
+  const [activeOverlay, setActiveOverlay] = useState(null);
+
+  const handleViewClick = (index) => {
+    setActiveOverlay(index);
+  };
+
+  const handleCloseClick = () => {
+    setActiveOverlay(null);
+  };
+
+  const images = [pic1, pic2, pic3, pic4, pic5, pic6];
+
   return (
-    <div>
-      <h1>Gallery</h1>
+    <div className="gallery">
+      {images.map((src, index) => (
+        <div className="gallery-item" key={index}>
+          <img
+            className="gallery-image"
+            src={src}
+            alt={`Gallery item ${index + 1}`}
+            onClick={() => handleViewClick(index)}
+          />
+          {activeOverlay === index && (
+            <div className="overlay">
+              <div className="overlay-inner">
+                <button className="close" onClick={handleCloseClick}>
+                  Close X
+                </button>
+                <img
+                  className="overlay-image"
+                  src={src}
+                  alt={`Overlay item ${index + 1}`}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
